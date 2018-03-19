9da3d7ddcbd8f179c1c7b4cf6898148d0d9ceba0
Noel Serrato <Serrato@LGMBP.local>
2018-03-19 14:57:14 -0700

    Noel : fixed index route

--- config/routes.js
+++ config/routes.js
@@ -2,7 +2,7 @@ let index = require('../controllers/index.js');
 let login = require('../controllers/login.js');
 
 module.exports = function(app){
-  app.use('/index',index.home);
+  app.get('/',index.home);
+  app.get('/login',login.login);
 
-  app.use('/login',login.login);
 }
