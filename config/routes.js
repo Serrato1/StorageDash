8f47d56c66a3ea6f5f898cd90c357a3d3b2ee75f
Noel Serrato <Serrato@LGMBP.local>
2018-03-21 10:27:19 -0700

    Noel : Listing

--- config/routes.js
+++ config/routes.js
@@ -1,8 +1,18 @@
 let index = require('../controllers/index.js');
 let login = require('../controllers/login.js');
+let listings = require('../controllers/listings.js');
 
 module.exports = function(app){
-  app.get('/',index.home);
-  app.get('/login',index.login);
 
+  app.get('/',index.home);
+  app.get('/login',login.login);
+  app.post('/login',login.authenticate);
+  app.get('/signup',index.signup);
+
+  app.use('/auth', validate)
+  app.get('/auth/listings', listings.home)
+}
+
+const validate = (req, res, next) => {
+  req.session.user ? next() : res.redirect('/');
 }
